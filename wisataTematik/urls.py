91e47ab077b309b24c28ff6b973654ea916a340a
sopigoo <shofinadhifa24@gmail.com>
2025-04-19 21:01:50 +0700

    feat: add edit url

--- wisataTematik/urls.py
+++ wisataTematik/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import tambah_rute_wisata, detail_rute_wisata
+from .views import tambah_rute_wisata, detail_rute_wisata, edit_rute_wisata
 
 urlpatterns = [
     path('tambah/', tambah_rute_wisata, name='tambah_rute'),
     path('<int:pk>/', detail_rute_wisata, name='detail_rute'),
+    path('<int:pk>/edit/', edit_rute_wisata, name='edit_rute'),
 ]
