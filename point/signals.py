c97a11206856281bbbcd544f25904a70d7e67c74
deanitasekar <deanitasekar@gmail.com>
2025-04-19 15:57:16 +0700

    delete: point app

--- point/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from review.models import Review
-
-@receiver(post_save, sender=Review)
-def add_points_on_review(sender, instance, created, **kwargs):
-    if created:
-        user_profile = instance.user.profile
-        user_profile.add_points(5)
--- point/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('redeem_coupon/<int:coupon_id>/', views.redeem_coupon, name='redeem_coupon'),
-]
