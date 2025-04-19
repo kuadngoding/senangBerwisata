8d5c6c9176ff5b0e918e24445ac463c2e29dddca
sopigoo <shofinadhifa24@gmail.com>
2025-04-19 20:21:12 +0700

    feat: add wisata tematik urls

---
+++ wisataTematik/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import tambah_rute_wisata, detail_rute_wisata
+
+urlpatterns = [
+    path('tambah/', tambah_rute_wisata, name='tambah_rute'),
+    path('<int:pk>/', detail_rute_wisata, name='detail_rute'),
+]
