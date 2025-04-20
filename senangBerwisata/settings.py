16578be578c38c2b7cf31d8b0e19a6eff3cb4dd5
AlyssaLayla <alyssasasti@gmail.com>
2025-04-20 23:55:12 +0700

    fix: settings.py

--- senangBerwisata/settings.py
+++ senangBerwisata/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vfxn&)7s%8ze&=!v81@haeb0_fmh)hg4pwfk^0a42nq0g=r+6%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "senang-berwisata.up.railway.app", "vital-gabbey-alyssalayla-72f2fa98.koyeb.app"]
 
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'koyebdb',
         'USER': 'koyeb-adm',
-        'PASSWORD': os.getenv('DB_PASSWORD') or 'MISSING',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': 'ep-square-mud-a2hck1ad.eu-central-1.pg.koyeb.app',
         'PORT': '5432',
         'OPTIONS': {'sslmode': 'require'},
